9661833898c5416d0c3d2a543ce9e73ce8504409
syahridho arjuna syahputra <syahridhosyahputra@gmail.com>
2024-12-03 01:07:37 +0700

    change baseURL

--- src/lib/axios/instance.ts
+++ src/lib/axios/instance.ts
@@ -7,8 +7,27 @@ const headers = {
   Exprires: 0,
 };
 
+let baseURL = "";
+
+if (typeof window !== "undefined") {
+  const hostname = window.location.hostname;
+
+  if (hostname === "localhost") {
+    baseURL = "https://localhost:3000";
+  } else if (
+    hostname === "syahridho.web.id" ||
+    hostname === "www.syahridho.web.id"
+  ) {
+    baseURL = "https://syahridho.web.id";
+  } else if (hostname === "portfolio-ten-liard-53.vercel.app") {
+    baseURL = "https://portfolio-ten-liard-53.vercel.app";
+  } else {
+    baseURL = "https://syahridho.web.id";
+  }
+}
+
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL,
   headers,
 });
 
