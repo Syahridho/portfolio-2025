5d459ae9f7eb949155a7b0657accc17acc65f285
syahridho arjuna syahputra <syahridhosyahputra@gmail.com>
2024-12-03 23:02:54 +0700

    fix axios

--- src/lib/axios/instance.ts
+++ src/lib/axios/instance.ts
@@ -1,33 +1,33 @@
 import axios from "axios";
 
-// const headers = {
-//   Accept: "application/json",
-//   "Content-Type": "application/json",
-//   "Cache-Control": "no-cache",
-//   Expires: 0,
-// };
-
-const getBaseURL = () => {
-  const hostname =
-    typeof window !== "undefined" ? window.location.hostname : "";
-
-  if (hostname === "syahridho.web.id") {
-    return "https://syahridho.web.id";
-  } else if (hostname === "www.syahridho.web.id") {
-    return "https://www.syahridho.web.id";
-  } else if (hostname === "portfolio-ten-liard-53.vercel.app") {
-    return "https://portfolio-ten-liard-53.vercel.app";
-  } else if (hostname === "localhost") {
-    return "http://localhost:3000";
-  }
-
-  // Default fallback
-  return "https://syahridho.web.id";
+const headers = {
+  Accept: "application/json",
+  "Content-Type": "application/json",
+  "Cache-Control": "no-cache",
+  Expires: 0,
 };
 
+// const getBaseURL = () => {
+//   const hostname =
+//     typeof window !== "undefined" ? window.location.hostname : "";
+
+//   if (hostname === "syahridho.web.id") {
+//     return "https://syahridho.web.id";
+//   } else if (hostname === "www.syahridho.web.id") {
+//     return "https://www.syahridho.web.id";
+//   } else if (hostname === "portfolio-ten-liard-53.vercel.app") {
+//     return "https://portfolio-ten-liard-53.vercel.app";
+//   } else if (hostname === "localhost") {
+//     return "http://localhost:3000";
+//   }
+
+//   // Default fallback
+//   return "https://syahridho.web.id";
+// };
+
 const instance = axios.create({
-  baseURL: getBaseURL(),
-  // headers,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers,
 });
 
 export default instance;
